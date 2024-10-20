d6425c5c9d5af2f2c3f204b6ec023905c882caf7
GITHUB520520 <84057766+GITHUB520520@users.noreply.github.com>
2024-10-20 18:05:50 +0800

    add 不同序列化方式的spiLoader

--- rpc-core/src/main/java/com/project/Main.java
+++ rpc-core/src/main/java/com/project/Main.java
@@ -1,7 +1,19 @@
 package com.project;
 
+import com.project.serializer.Serializer;
+
+import java.util.ServiceLoader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 指定序列化器
+        Serializer serializer = null;
+        ServiceLoader<Serializer> serviceLoader = ServiceLoader.load(Serializer.class);
+        for (Serializer service : serviceLoader) {
+            serializer = service;
+            System.out.println(serializer);
+        }
+
     }
 }
